28eaea8274408ec4d798feff0b612ada4752fdf0
dany14101 <danylinan74@gmail.com>
2025-04-27 20:02:53 +0000

    cambio 5

--- Codigo.py
+++ Codigo.py
@@ -13,7 +13,7 @@ def grafico(punto_ini,error=1e-3,limite=50):
     return float(act)
 
 
-#Codigo para el metofo biseccion
+#Codigo para el metodo biseccion
 def biseccion(a,b,error=1e-3,limite=50):
     p=np.poly1d([4,5,1])
     for _ in range(limite):
@@ -67,3 +67,79 @@ def newton(x0,error=1e-3,limite=50):
     return (x1)
 
 #Codigo para el metodo de secante
+def secante(x0,error=1e-3,limite=50):
+    f= lambda x:5*x**3+10*x+5  
+    x1=x0+0.1   
+    for _ in range(limite):
+        fx0=f(x0)
+        fx1=f(x1)
+        x2=x1-fx1*(x1-x0)/(fx1-fx0)
+        if abs(x2-x1)<error:
+            return (x2)
+        x0=x1
+        x1=x2
+    return (x1)
+
+#Codigo para gauss jordan
+def gaussjordan(M1,M2):
+    M1=np.array(M1,dtype=float)
+    M2=np.array(M2,dtype=float).reshape(-1, 1)
+    RES=np.hstack([M1,M2])
+    n=len(M2)
+    
+    for i in range(n):
+        RES[i]=RES[i]/RES[i][i]
+        for j in range(n):
+            if i!=j:
+                RES[j]=RES[j]-RES[j][i]*RES[i]
+    
+    return (RES[:,-1])
+
+#Codigo para resolver por matriz diagonal superior
+def diago_inferior(M1,M2):
+    tam=len(M2)
+    x=[0]*tam
+    for i in range(tam):
+        tot=sum(M1[i][j]*x[j] for j in range(i))
+         x[i]=(M2[i]-tot)/M1[i][i]
+    return (x)
+
+# Metodo de jacobi
+def jacobi(A, b,x0=None,error=1e-3,limite=50):
+    A=np.array(A,dtype=float)
+    B=np.array(b,dtype=float)
+    tam=len(b) 
+    if x0 is None:
+        x0=np.zeros(tam)
+    arr=x0.copy()
+    for _ in range(limite):
+        nuevo=np.zeros_like(arr)
+        for i in range(tam):
+            suma=sum(A[i][j]*arr[j] for j in range(tam) if j!=i)
+            nuevo[i]=(b[i]-suma)/A[i][i]
+        if np.linalg.norm(nuevo-arr,ord=np.inf)<error:
+            return nuevo
+        arr=nuevo.copy()
+    return (arr)
+
+# Metodo de Gaus seidel
+def gaussseidel(A,b,x0=None,error=1e-3,limite=50):
+    # convierte a flotantes
+    A=np.array(A,dtype=float)
+    B=np.array(b,dtype=float)
+    tam=len(b)
+    # checa que caso es
+    if x0 is None:
+        x0=np.zeros(tam)
+    arr=x0.copy()
+    # hace las iteraciones
+    for _ in range(limite):
+        # guarda el anterior
+        anterior=arr.copy()
+        for i in range(tam):
+            suma=sum(A[i][j]*arr[j] for j in range(tam) if j!=i)
+            arr[i]=(b[i]-suma)/A[i][i]
+        # checa si se cumplio la condicion y ve el cambio
+        if np.linalg.norm(arr-anterior,ord=np.inf)<error:
+            return (arr)
+    return (arr)
